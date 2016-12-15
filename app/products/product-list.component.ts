24ecc72d9cbe1af3a5b8c302ce8ae2e245908ae8
srichmond3000 <srichmond3000@googlemail.com>
2016-12-15 10:10:59 +0000

    Added nested component with inputs and outputs.

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -43,4 +43,8 @@ export class ProductListComponent implements OnInit{
     ngOnInit(): void {
         console.log('In OnInit');
     }
+
+    onRatingClicked(message: string): void{
+        this.pageTitle = 'Product List: ' + message;
+    }
 }
